2a35d6a159968983c6339a093cc4b725d8593e7b
Nabil <cleo.master0000@gmail.com>
2024-07-18 22:27:03 +0500

    init

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { hello } from './modules/my-module';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  useEffect(() => {
+    console.log(hello());
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
